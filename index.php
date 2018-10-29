3dacc80e6fabf3fe872972cfcd9273d77e7b1401
Lucas <lucas180395@gmail.com>
2018-10-29 13:31:09 +0100

    correction du formulaire copie

--- index.php
+++ index.php
@@ -7,45 +7,38 @@
     <title>Tables de Multiplication</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
-<form method="post" action="">
-<select name="first_dropdown">
-<option value="a">a</option>
-<option value="b">b</option>
-<option value="c">c</option>
-</select>
-<input type="submit" />
-</form>
-<?php 
-if (isset($_POST['first_dropdown']) && !empty($_POST['first_dropdown']) ):
-?>
-
-
-<select name="second_dropdown">
-
-<?php
-//if user select a
-if ($_POST['first_dropdown'] == "a"):
-?>
-<option value="a1">a1</option>
-<option value="a2">a2</option>
-<option value="a3">a3</option>
-<?php endif; ?>
-
+<p>
+<form action="" method="post">
+	Choisissez la table
+    <select name="multi">
+	  <option value="1">1</option>
+	  <option value="2">2</option>
+	  <option value="3">3</option>
+	  <option value="4">4</option>
+	  <option value="5">5</option>
+	  <option value="6">6</option>
+	  <option value="7">7</option>
+	  <option value="8">8</option>
+	  <option value="9">9</option>
+	  <option value="10">10</option>
+	</select>
+    <input type="submit" value="Calculer">
+    </form>
+</p>
 
 <?php
+    $multi = $_POST['multi'];
+    echo "<p>Table du " . $multi . " :</p>";
 
- $multi = 3;
-
-for ($i=1; $i <=10 ; $i++) { 
-    echo "<p>" . $i . "x" . $multi . "=" . $i*$multi . "</p>"; 
-}
-
-?>
-
-
-
+        for ($i=1; $i <= 10 ; $i++) { 
+            echo "<p>" . $i . "x" . $multi . "=" . $i*$multi . "</p>"; 
+        }
     
+	?>
+
 </body>
+
 </html>
