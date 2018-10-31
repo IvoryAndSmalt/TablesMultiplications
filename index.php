873d39241ccf9ec99b7d164cb29497fc691afa70
Lucas <lucas180395@gmail.com>
2018-10-31 14:42:29 +0100

    commit mercredi

--- index.php
+++ index.php
@@ -5,18 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tables de Multiplication</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.13/css/mdb.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:700|Poppins" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+
 </head>
 
 <body>
 
-    <h1 id="nomApplication">
-        Le nom de l'application
-    </h1>
 
-    <a href="tables.php" id="lestables" class="nosboutons">Les tables</a>
-    <a href="revisiontables.php" class="nosboutons" id="reviser">Réviser !</a>
+    <div class="container">
+
+        <div class="row justify-content-center">
+            <h1 id="nomApplication" class="">
+                MULTIWOW
+            </h1>
+        </div>
+        <div class="row justify-content-center">
+            <p class="slogan col-10">
+                Apprenez les tables de multiplication en vous amusant !
+            </p>
+        </div>
+        <div class="row justify-content-center">
+            <a class="diva col-6 btn btn-vert" href="tables.php">Les tables</a>
+            <a class="diva col-6 btn btn-orange" href="revisiontables.php">Réviser !</a>
+        </div>
+    </div>
+
+
 
 </body>
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.13/js/mdb.min.js"></script>
 
 </html>
--- revision.php
+++ revision.php
@@ -6,6 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tables de Multiplication</title>
     <link rel="stylesheet" href="css/style.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.13/css/mdb.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
